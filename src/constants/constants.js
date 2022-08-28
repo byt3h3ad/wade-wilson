bdd8871eafb919a87407215a3c937f3fa6f17af5
bytehead <adhirajd007@gmail.com>
2022-08-28 20:35:23 +0530

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'Deadpool Kills the Marvel Universe',
+    description: "Had a swell time killing off each superhero and supervillain thanks to my deterioting mental health. Even almost found you too. Better luck next time?",
+      image: 'https://i.annihil.us/u/prod/marvel/i/mg/e/03/519273427d5e7/standard_incredible.jpg',
+      tags: ['Swords', 'Bolt Action Rifle', 'Revolver', 'Therapy'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'Deadpool Killustrated',
+    description:"The og multiverse of madness. Killed off each classic character that inspired the Marvel characters.",
+    image: 'https://i.annihil.us/u/prod/marvel/i/mg/9/60/5228f51bcd67b/standard_incredible.jpg',
+    tags: ['Nihilism', 'Bazookas'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'Deadpool kills Deadpool',
+    description: "I went full dreadpool and set out to kill myself everywhere. Nuts.",
+      image: 'https://i.annihil.us/u/prod/marvel/i/mg/a/70/53879daf69eb5/standard_incredible.jpg',
+      tags: ['Self-healing', 'mental illness'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'House of M',
+    description: "Worked as a field commander and active agent of S.H.I.E.L.D.",
+    image: 'https://i.annihil.us/u/prod/marvel/i/mg/3/b0/589e106eb6191/detail.jpg',
+    tags: ['Assassin', 'Merc', 'Crackhead'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: 'Started my journey', },
+  { year: 2018, text: 'Worked as a freelance developer', },
+  { year: 2019, text: 'Became a recognised merc', },
+  { year: 2020, text: 'Shared my projects with the world', },
+  { year: 2021, text: 'Started my own platform', },
+];
